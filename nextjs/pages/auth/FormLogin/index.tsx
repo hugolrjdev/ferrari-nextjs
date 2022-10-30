d38f3290b12a2663916f3c5d402250fbfb6c7878
hugolrjdev <fortalezaplanodesaude@gmail.com>
2022-10-30 20:17:21 -0300

    exibindo erros do formulario

--- nextjs/pages/auth/FormLogin/index.tsx
+++ nextjs/pages/auth/FormLogin/index.tsx
@@ -1,21 +1,34 @@
 import axios from "axios";
+import { get } from "lodash";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import Alert from "../../../components/Alert";
 
 type FormData = {
   email: string;
   password:string;
+  server?: string;
 }
 
 const FormLogin = () => {
-  const {register, handleSubmit} = useForm<FormData>();
+  const {register, handleSubmit, formState: {errors}, setError, clearErrors, watch} = useForm<FormData>();
+
+  const email = watch('email');
+  const password = watch('password');
 
   const enviar = (data: FormData) => {
     axios.post(`/auth/login`, data, {
       baseURL: process.env.API_URL,
     }).then(({data})=>{
       console.log(data)
-    }).catch((err)=>{console.error(err)})
+    }).catch((err: any)=>setError('server', {
+      message: err.response.data.message ?? err.message ?? 'Error Desconhecido'
+    }));
   }
+
+  useEffect(()=>{
+    clearErrors();
+  }, [email, password, clearErrors])
 
     return(
         <form id="auth-login" onSubmit={handleSubmit(enviar)}>
@@ -23,13 +36,15 @@ const FormLogin = () => {
 
             <hr />
 
+            {errors && Object.keys(errors).length > 0 && <Alert type="danger">{Object.keys(errors).map((key, index) => <div key={index}>{get(errors, `${key}.message`)}&nbsp;</div>)}</Alert>}
+
             <div className="field">
-              <input type="email" id="email-login" {...register('email',{required: 'E=mail é Obrigatório'})} />
+              <input type="email" id="email-login" {...register('email',{required: 'E=mail é Obrigatório.'})} />
               <label htmlFor="email-login">E-mail</label>
             </div>
 
             <div className="field">
-              <input type="password" id="password" {...register('password', {required: 'Password é obrigatório'})} />
+              <input type="password" id="password" {...register('password', {required: 'Password é obrigatório.'})} />
               <label htmlFor="password">Digite sua Senha</label>
             </div>
 
